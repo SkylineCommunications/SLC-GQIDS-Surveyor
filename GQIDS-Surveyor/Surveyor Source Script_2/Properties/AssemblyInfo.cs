f76d78be05c93b048747d04d8df9bc1ad9682421
Ronald Gerard <ronald.gerard@skyline.be>
2023-03-08 14:00:26 +0100

    Added automation script solution

---
+++ GQIDS-Surveyor/Surveyor Source Script_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Surveyor Source Script_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Surveyor Source Script_2")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("549B270F-96EE-4F02-B9DC-5F36F6807D1B")]
+[assembly: AssemblyVersion("1.0.0.0")]
